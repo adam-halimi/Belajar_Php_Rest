b4d0dd48b95fa27c28c794eb2e0ef504330ffb49
adam-halimi <emailnya.addam@gmail.com>
2019-03-12 10:26:52 +0700

    complete file db

--- Config/Database.php
+++ Config/Database.php
@@ -12,9 +12,11 @@
             $this->conn = null;
 
             try {
-
+                $this->conn = new PDO('mysql:host='.$this->host.';dbname='.$this->db.';charset=utf8', $this->username, $this->password);
+                $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             } catch(PDOException $e) {
-                
+                echo 'Connection Error : ' . $e->getMessage();
             }
+            return $this->conn;
         }
     }
